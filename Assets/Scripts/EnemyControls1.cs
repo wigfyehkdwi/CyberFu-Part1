e2e3612ebee28814958873beb26d3d2e3e0b5785
skyefox1236 <149954126+skyefox1236@users.noreply.github.com>
2024-06-01 12:38:34 -0400

    e

--- Assets/Scripts/EnemyControls1.cs
+++ Assets/Scripts/EnemyControls1.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 public class EnemyControls1 : MonoBehaviour
 {
     public float speed = 5;
-    public float attackingDistance = 9/11;
-    public Vector3 al_qaedirection;
+    public float attackingDistance = 1;
+    public Vector3 direction;
+
+    private Animator animatorEnemy;
+    private Rigidbody rigidbodyEnemy;
+    private Transform target;
+    private bool isFollowingTarget;
+    private bool isAttackingTarget;
+    private float chasingPlayer = 0.01f;
+    private float currentAttackingTime;
+    private float maxAttackingTime = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isFollowingTarget = true;
+        currentAttackingTime = maxAttackingTime;
+        animatorEnemy = GetComponent<animatorEnemy>();
+        rigidbodyEnemy = GetComponent<rigidbodyEnemy>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
