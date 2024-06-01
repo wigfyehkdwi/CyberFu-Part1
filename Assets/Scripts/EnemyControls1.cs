5c6b5f514877c4511283af288324f1307bc4ec45
skyefox1236 <gillarstar12@gmail.com>
2024-06-01 11:48:07 -0400

    im osama
    run tell obama

---
+++ Assets/Scripts/EnemyControls1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControls1 : MonoBehaviour
+{
+    public float speed = 5;
+    public float attackingDistance = 9/11;
+    public Vector3 al_qaedirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
