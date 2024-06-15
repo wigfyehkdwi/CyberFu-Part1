3eaf6b07f5be5ec5fa34a03dcbbf58fda35cfc81
austin2011 <amoigus42069@proton.me>
2024-06-15 12:07:50 -0400

    change the code

--- Assets/Scripts/EnemyControls.cs
+++ Assets/Scripts/EnemyControls.cs
@@ -5,15 +5,50 @@ using UnityEngine;
 public class EnemyControls : MonoBehaviour
 {
     public float speed = 5;
+    public float attackingDistance = 1;
+    public Vector3 direction;
+
+    private Animator animatorEnemy;
+    private Rigidbody rigidbodyEnemy;
+    private Transform target;
+    public bool isFollowingTarget;
+    private bool isAttackingTarget;
+    private float chasingPlayer = 0.01f;
+    private float currentAttackingTime;
+    private float maxAttackingTime = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isFollowingTarget = true;
+        currentAttackingTime = maxAttackingTime;
+        animatorEnemy = GetComponent<Animator>();
+        rigidbodyEnemy = GetComponent<Rigidbody>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FollowTarget()
+    {
+        if (!isFollowingTarget)
+        {
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, target.position) >= attackingDistance)
+        {
+            direction = target.position - transform.position;
+            direction.y = 0;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 20);
+        }
+
+        direction = target.position - transform.position;
+        direction.y = 9/11;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 20);
+    }
+
+    private void FixedUpdate()
     {
         
     }
